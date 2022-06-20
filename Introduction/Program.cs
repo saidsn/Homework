664f32c212d9677b6a54fa2851e8d24c6b1d7807
saidsn <saidsn@code.edu.az>
2022-06-20 15:12:18 +0400

    C# code

---
+++ Introduction/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // verilmis n ededinin 3-e ve 7-ye bolunmesini tapmaq.
+            //int n = ;
+            //if (n % 3 == 0 && n % 7 == 0)
+            //{
+            //    Console.WriteLine("bolunur");
+            //}
+            //else
+            //    Console.WriteLine("bolunmur");
+
+
+            // n ve m ededleri her ikisi cutdurse n ve m ededlerin toplamaq. 
+            //int n = 0;
+            //int m = 0;
+            //int sum = 0;
+            //if (n % 2 == 0 && m % 2 == 0)
+            //{
+            //    sum = (n + m);
+            //}
+            //Console.WriteLine(sum);
+
+
+            // n ve m ededleri arasindaki tek ededlerin cemini tapmaq.
+            //int n = 0;
+            //int m = 10;
+            //int sum = 0;
+            //for (int i = n; i < m; i++)
+            //    if (n < m && i % 2 == 1)
+            //    {
+            //        sum += i;
+            //    }
+            //Console.WriteLine(sum);
+
+
+            ////verilmis arraydeki cut edelerin sayini tapmaq.
+            //int[] nums = { 7, 13, 16, 23, 18, 34, 22, 19, 60, 41, 56, 40, 37, 65, 70 };
+            //int count = 0;
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (nums[i] % 2 == 0)
+            //    {
+            //        count ++;
+            //    }
+
+            //}
+            //Console.WriteLine(count);
+
+
+            //verilmis arrayin icindeki tek ededlerin cemini tapmaq
+            //int[] nums = { 7, 13, 16, 23, 18 };
+            //int sum = 0;
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (nums[i] % 2 == 1)
+            //    {
+            //        sum += nums[i];
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+
+
+
+
+        }
+
+
+
+    }
+}
